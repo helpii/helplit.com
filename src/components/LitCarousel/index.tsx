0a54e054669a6e08556e33b4454edd1faec4d12a
JM Fantin <jmfantin@outlook.com>
2023-08-16 01:43:05 -0300

    carousel improvements

--- src/components/LitCarousel/index.tsx
+++ src/components/LitCarousel/index.tsx
@@ -1,7 +1,7 @@
 import { ArrowLeft, ArrowRight } from "phosphor-react";
 import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react";
 import { PropsWithChildren, useState, useEffect } from "react";
-import classNames from "classnames";
+import shapeshifter from "classnames";
 
 // Define the props
 type CarouselProps = PropsWithChildren & EmblaOptionsType;
@@ -29,60 +29,76 @@ export const LitCarousel = ({ children, ...options }: CarouselProps) => {
 
   const canScrollNext = !!emblaApi?.canScrollNext();
   const canScrollPrev = !!emblaApi?.canScrollPrev();
+
   return (
-    <>
-      <div className="overflow-hidden" ref={emblaRef}>
-        <div className="flex">{children}</div>
-      </div>
+    <div className="flex flex-row">
       <CarouselControls
+        side="left"
         canScrollNext={canScrollNext}
         canScrollPrev={canScrollPrev}
         onNext={() => emblaApi?.scrollNext()}
         onPrev={() => emblaApi?.scrollPrev()}
       />
-    </>
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="flex">{children}</div>
+      </div>
+      <CarouselControls
+        side="right"
+        canScrollNext={canScrollNext}
+        canScrollPrev={canScrollPrev}
+        onNext={() => emblaApi?.scrollNext()}
+        onPrev={() => emblaApi?.scrollPrev()}
+      />
+    </div>
   );
 };
 
 type ControlsProps = {
+  side: string;
   canScrollPrev: boolean;
   canScrollNext: boolean;
   onPrev(): void;
   onNext(): void;
 };
 const CarouselControls = (props: ControlsProps) => {
-  return (
-    <div className="flex justify-end gap-2 ">
-      <button
-        onClick={() => {
-          if (props.canScrollPrev) {
-            props.onPrev();
-          }
-        }}
-        disabled={!props.canScrollPrev}
-        className={classNames({
-          "px-4 py-2 text-white rounded-md": true,
-          "bg-indigo-200": !props.canScrollPrev,
-          "bg-indigo-400": props.canScrollPrev,
-        })}
-      >
-        Prev
-      </button>
-      <button
-        onClick={() => {
-          if (props.canScrollNext) {
-            props.onNext();
-          }
-        }}
-        disabled={!props.canScrollNext}
-        className={classNames({
-          "px-4 py-2 text-white rounded-md": true,
-          "bg-indigo-200": !props.canScrollNext,
-          "bg-indigo-400": props.canScrollNext,
-        })}
-      >
-        Next
-      </button>
-    </div>
-  );
+  switch (props.side) {
+    case "left":
+      return (
+        <button
+          onClick={() => {
+            if (props.canScrollPrev) {
+              props.onPrev();
+            }
+          }}
+          disabled={!props.canScrollPrev}
+          className={shapeshifter({
+            "px-4 py-2": true,
+            "bg-NUDE": !props.canScrollNext,
+            "bg-[#FCF9F7]": props.canScrollNext,
+          })}
+        >
+          <ArrowLeft size={32} color="#f27d54" weight="duotone" />
+        </button>
+      );
+    case "right":
+      return (
+        <button
+          onClick={() => {
+            if (props.canScrollNext) {
+              props.onNext();
+            }
+          }}
+          disabled={!props.canScrollNext}
+          className={shapeshifter({
+            "px-4 py-2": true,
+            "bg-NUDE": !props.canScrollNext,
+            "bg-[#FCF9F7]": props.canScrollNext,
+          })}
+        >
+          <ArrowRight size={48} color="#f27d54" weight="duotone" />
+        </button>
+      );
+    default:
+      return <h1>error</h1>;
+  }
 };
