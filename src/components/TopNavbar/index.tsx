9c8fe9389e22fdc7acdbb72ca3a6d3a406fd1eb0
JM Fantin <jmfantin@outlook.com>
2023-08-18 13:43:54 -0300

    best navbar

--- src/components/TopNavbar/index.tsx
+++ src/components/TopNavbar/index.tsx
@@ -1,16 +1,16 @@
 export function TopNavbar() {
   return (
-    <nav className="bg-white border-gray-200 w-[100%]">
+    <nav className="bg-white border-gray-200 w-[100%] md:pl-12 md:pr-14 select-none">
       <div className="flex flex-row items-center justify-between p-2 font-TEXTO">
         <a href="https://helplit.com.br/" className="flex items-center">
-          <img src="hlt-navbar.svg" className="h-10 mr-3" alt="Helplit Logo" />
+          <img src="hlt-navbar.svg" className="h-10" alt="Helplit Logo" />
         </a>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white ">
+          <ul className="hidden md:flex font-medium flex-row md:gap-x-8 md:mt-0 md:border-0 md:bg-white ">
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900  rounded md:bg-transparent md:hover:text-CORAL md:p-0  "
+                className="text-COAL hover:text-CORAL transition-colors ease-in-out"
                 aria-current="page"
               >
                 Blog
@@ -19,7 +19,7 @@ export function TopNavbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-CORAL md:p-0"
+                className="text-COAL hover:text-CORAL transition-colors ease-in-out"
               >
                 Preços
               </a>
@@ -27,7 +27,7 @@ export function TopNavbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-CORAL md:p-0"
+                className="text-COAL hover:text-CORAL transition-colors ease-in-out"
               >
                 Sobre nós
               </a>
@@ -35,12 +35,23 @@ export function TopNavbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-CORAL md:p-0"
+                className="text-COAL hover:text-CORAL transition-colors ease-in-out"
               >
                 Como funciona
               </a>
             </li>
           </ul>
+        </div>
+        <div className="flex flex-row gap-x-8 items-center">
+          <a
+            href="#"
+            className="hidden md:block text-COAL hover:text-CORAL transition-colors ease-in-out"
+          >
+            Entrar
+          </a>
+          <button className="mr-2 md:mr-0 text-CORAL border-2 border-CORAL py-1 px-4 rounded-full hover:bg-CORAL hover:text-white transition-colors ease-in-out">
+            Comece a planejar
+          </button>
         </div>
       </div>
     </nav>
