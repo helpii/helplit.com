a253dd591b9bfaeec3e79715da3e0cf3bff2143f
JM Fantin <jmfantin@outlook.com>
2023-09-20 09:26:47 -0300

    leftover gambi

--- src/components/Manutencao/index.tsx
+++ src/components/Manutencao/index.tsx
@@ -2,32 +2,44 @@ export function Manutencao() {
   return (
     <div className="bg-[#ffd52d] h-auto py-2 m-auto overflow-hidden relative w-auto">
       <ul className="flex w-[calc(700px*11)] animate-scroll">
-        <li className="w-[700px]">estamos em manutenção :)</li>
+        <li className="w-[700px]">
+          <i>ㅤㅤㅤㅤㅤㅤㅤ</i>estamos em manutenção :)
+        </li>
         <li className="w-[700px]">
           enquanto isso, acesse{' '}
           <span className="font-bold">helplit.glide.page</span>
         </li>
-        <li className="w-[700px]">estamos em manutenção :)</li>
+        <li className="w-[700px]">
+          <i>ㅤㅤㅤㅤㅤㅤㅤ</i>estamos em manutenção :)
+        </li>
         <li className="w-[700px]">
           enquanto isso, acesse{' '}
           <span className="font-bold">helplit.glide.page</span>
         </li>
-        <li className="w-[700px]">estamos em manutenção :)</li>
+        <li className="w-[700px]">
+          <i>ㅤㅤㅤㅤㅤㅤㅤ</i>estamos em manutenção :)
+        </li>
         <li className="w-[700px]">
           enquanto isso, acesse{' '}
           <span className="font-bold">helplit.glide.page</span>
         </li>
-        <li className="w-[700px]">estamos em manutenção :)</li>
+        <li className="w-[700px]">
+          <i>ㅤㅤㅤㅤㅤㅤㅤ</i>estamos em manutenção :)
+        </li>
         <li className="w-[700px]">
           enquanto isso, acesse{' '}
           <span className="font-bold">helplit.glide.page</span>
         </li>
-        <li className="w-[700px]">estamos em manutenção :)</li>
+        <li className="w-[700px]">
+          <i>ㅤㅤㅤㅤㅤㅤㅤ</i>estamos em manutenção :)
+        </li>
         <li className="w-[700px]">
           enquanto isso, acesse{' '}
           <span className="font-bold">helplit.glide.page</span>
         </li>
-        <li className="w-[700px]">estamos em manutenção :)</li>
+        <li className="w-[700px]">
+          <i>ㅤㅤㅤㅤㅤㅤㅤ</i>estamos em manutenção :)
+        </li>
       </ul>
     </div>
   );
